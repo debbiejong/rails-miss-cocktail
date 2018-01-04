0438c36653bc4c7bb70d7391070eb47e7cc4f894
Debbie Jong <surrealstardust@gmail.com>
2018-01-04 11:56:51 +0900

    fix routes, update controllers, and add views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :cocktails
+  resources :cocktails do
+    resources :doses, only: [:new, :create]
+  end
+
+  resources :doses, only: [:destroy]
+  # get "cocktails/new", to: "cocktails#new"
+  # post "cocktails", to: "cocktails#create"
+
+  # get "cocktails", to: "cocktails#index"
+  # get "cocktails/:id", to: "cocktails#show"
+
+  # get "cocktails/:id/edit", to: "cocktails#edit"
+  # patch "cocktails/:id", to: "cocktails#update"
+
+  # delete "cocktails/:id", to: "cocktails#destroy"
 end
